1f82f0104b50f85cc0175eee197912e8fddd6fad
surceBemBo <106420621+surceBemBo@users.noreply.github.com>
2022-06-20 08:35:43 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import asyncio
+from pytgcalls import idle
+from driver.veez import call_py, bot, user
+
+
+async def start_bot():
+    await bot.start()
+    print("[INFO]: BOT & UBOT CLIENT STARTED !!")
+    await call_py.start()
+    print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
+    await user.join_chat("UUBU0")
+    await user.join_chat("B_e_m_0")
+    await user.join_chat("UUF8U")
+    await idle()
+    print("[INFO]: STOPPING BOT & USERBOT")
+    await bot.stop()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
