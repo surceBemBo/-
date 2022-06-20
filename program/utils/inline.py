ec20bd30d4fb1845085a704089fac44c123af321
surceBemBo <106420621+surceBemBo@users.noreply.github.com>
2022-06-20 08:26:03 +0200

    Add files via upload

---
+++ program/utils/inline.py
@@ -0,0 +1,63 @@
+""" inline section button """
+
+from pyrogram.types import (
+  CallbackQuery,
+  InlineKeyboardButton,
+  InlineKeyboardMarkup,
+  Message,
+)
+
+
+def stream_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="! الـقـائـمـه!", callback_data=f'cbmenu | {user_id}'),
+      InlineKeyboardButton(text="! الـتـحـديـثـات♭", url=fhttps://t.me/gebreelyad''),
+    ],
+    [
+    InlineKeyboardButton(
+                        "🐥اضـف الـبـوت لـمـجـمـوعـتـك🐥",
+                        url=f'https://t.me/Gebreel_1111bot?startgroup=true'),
+    ],
+  ]
+  return buttons
+
+
+def menu_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="⏹", callback_data=f'cbstop | {user_id}'),
+      InlineKeyboardButton(text="⏸", callback_data=f'cbpause | {user_id}'),
+      InlineKeyboardButton(text="▶️", callback_data=f'cbresume | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🔇", callback_data=f'cbmute | {user_id}'),
+      InlineKeyboardButton(text="🔊", callback_data=f'cbunmute | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🗑 اغلاق", callback_data='cls'),
+    ]
+  ]
+  return buttons
+
+
+close_mark = InlineKeyboardMarkup(
+  [
+    [
+      InlineKeyboardButton(
+        "🔙 رجوع", callback_data="cbmenu"
+      )
+    ]
+  ]
+)
+
+
+back_mark = InlineKeyboardMarkup(
+  [
+    [
+      InlineKeyboardButton(
+        "🔙 رجوع", callback_data="cbmenu"
+      )
+    ]
+  ]
+)
